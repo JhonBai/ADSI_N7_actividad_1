7c410a944e78d5eec162e78b7ce1871f77e4097d
Jhon Bairon Leon <jbleon54@misena.edu.co>
2019-07-30 21:27:05 -0500

    5to Avance

--- controlCEET/resources/views/All/navbar.blade.php
+++ controlCEET/resources/views/All/navbar.blade.php
@@ -1,7 +1,10 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: APRENDIZ
- * Date: 25/7/2019
- * Time: 21:17
- */
+<nav class="navcss">
+    <div class="nav-wrapper">
+        <div class="FontTitulo1"><a href="#" class="brand-logo center">SYNERGIAS MAGISTER</a></div>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><a href="{{ route('Home') }}">Inicio</a></li>
+
+        </ul>
+    </div>
+</nav>
+
--- controlCEET/resources/views/layout/layout.blade.php
+++ controlCEET/resources/views/layout/layout.blade.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: APRENDIZ
- * Date: 25/7/2019
- * Time: 21:15
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('libs/materialize/css/materialize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>ControlCEET</title>
+</head>
+<body >
+
+@include('All.navbar')
+
+<!-- Vistas del Home -->
+@yield('home')
+
+
+
+<script
+        src="https://code.jquery.com/jquery-3.4.1.js"
+        integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+        crossorigin="anonymous"></script>
+<script type="text/javascript" src="{{ asset('libs/materialize/js/materialize.js')}}"></script>
+<script type="text/javascript" src="{{ asset('js/app.js')}}"></script>
+</body>
+</html>
+
+
